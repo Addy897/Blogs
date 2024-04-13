6bc93f41ec99e9a6f13c9bf76d0a0b86b71d140d
Addy897 <145797684+Addy897@users.noreply.github.com>
2024-04-13 14:59:59 +0530

    initial Commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'verceldb',
+       'USER': 'default',
+       'PASSWORD': 'DT1lzxbJr4pM',
+       'HOST': 'ep-super-rain-a4ejq2rb-pooler.us-east-1.aws.neon.tech',
+       'PORT': '5432',
+   }
 }
 
 
